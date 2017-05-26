515feb5b0cf438d2b42abb27dab7e4ad956a0206
Tong Vuu <tong.vuu@terralogic.com>
2017-05-26 14:08:41 +0700

    Add simple line chat from canvasjs

--- web/test/testsystem.php
+++ web/test/testsystem.php
@@ -6,5 +6,5 @@
 	$dt_sysinfo = json_encode(array("module" => "sysinfo", "data" => ""));
 	$dt_gpio = json_encode(array("module" => "gpio", "data" => array("pin" => 1, "state" => 0)));
 
-	echo transceiver_data($dt_gpio);
+	echo transceiver_data($dt_sysinfo);
 ?>
