3f303939906ed236268621adcc9ed822c43332d1
SilverWolf <silverwolf@ceh.vn>
2019-05-05 16:04:34 +0100

    Cleanup

--- backend/test/data.py
+++
@@ -1,4 +0,0 @@
-# Sample data for testing
-linux = '{"module":"linux","data":{"cmd":"cat /etc/passwd"}}'
-gpio = '{"module":"gpio","data":{"pin":"18","state":"0"}}'
-sysinfo = '{"module":"sysinfo","data":{"info":"cpu"}}'
--- backend/util.py
+++
@@ -1,5 +0,0 @@
-import platform
-def is_rpi():
-	if platform.machine() == "armv6l" or platform.machine() == "armv7l":
-		return True
-	return False
